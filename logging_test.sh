2dd721db84b04e9d679d5e4bd5951cfa4030a563
Drees Dreessen <drees.dreessen@brille24.de>
2018-01-23 10:06:28 +0100

    Bug_Fixes

--- logging_test.sh
+++ logging_test.sh
@@ -2,20 +2,26 @@
 
 prog_log()
 {
+
 run="${program// /_}"
-    LOG_FILE=/var/log/$run.log
-    LOGTIME=`date "+%d-%m-%Y %H:%M:%S"`
-    if [ "$LOG_FILE" == "" ]; then
-    LOG=$LOGTIME;
-      else
-        LOG=$LOG_FILE
-    touch $LOG
-        if [ ! -f $LOG ]; then echo "ERROR!! Cannot create log file $LOG. Exiting."; exit 1; fi
-    echo $LOGTIME | tee -a $LOG;
-      fi
-echo " " | tee -a $LOG
-$program 2>&1 | tee -a $LOG 
+LOG_FILE=/var/log/"$run".log
+LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ "$LOG_FILE" == "" ]; then
+LOG="$LOGTIME";
+  else
+    LOG="$LOG_FILE"
+    touch "$LOG"
+    if [ ! -f "$LOG" ]; then echo "ERROR!! Cannot create log file "$LOG". Exiting."; exit 1;
+    fi
+      echo "$LOGTIME" | tee -a "$LOG";
+  fi
+echo " " | tee -a "$LOG"
+
+"$program" 2>&1 | tee -a "$LOG"
+echo "Log written at "$LOG""
 }
+
 echo "Run custom logger for Command:"
 read program
 prog_log
